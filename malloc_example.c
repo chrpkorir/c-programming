9e0b14acf48f62455a930f6a85e6c35b7473d4c6
chrpkorir <chrpkorir>
2022-04-05 08:38:32 +0000

    Malloc2

--- malloc_example.c
+++ malloc_example.c
@@ -18,3 +18,11 @@ int main(void)
 
 	return (0);
 }
+/**
+ * in the above malloc has been used to crea a 3 byte
+ * allocated space in memory, and we fill this space with
+ * characters
+ * @sizeof: allocate s3 times the size of a char(which
+ * happens to be 3 times 1 byte on 64-bit machine)
+ * Always use sizeof for better portability
+ */
