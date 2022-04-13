87df31849e4b5ad6401a6cc74710619593cca184
chrpkorir <faythkoko@gmail.com>
2022-04-13 20:35:38 +0000

    Function pointers

---
+++ function_pointer/example2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * fun - normal function with an int parameter and void return type
+ *
+ *
+ */
+void fun(int a)
+{
+	printf("Value of a is %d\n", a);
+}
+
+int main()
+{
+	void (*fun_ptr)(int) = fun; //& removed
+
+	fun_ptr(10); // * removed
+
+	return (0);
+}
